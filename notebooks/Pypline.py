3a86260d67996d22b2465b61415d458c673fc96e
Margarita <119706111+m-kachulyak@users.noreply.github.com>
2025-06-08 19:19:34 +0300

    Pypline.py

--- notebooks/Pypline.py
+++ notebooks/Pypline.py
@@ -1,3 +1,17 @@
+significant_features = ['price_object',
+ 'rooms_count',
+ 'area',
+ 'area_kitchen',
+ 'area_live',
+ 'area_live_ratio',
+ 'area_kitchen_ratio',
+ 'area_per_renovation',
+ 'year_buld',
+ 'h3_categorical',
+ 'floors_count',
+ 'distance_to_center',
+ 'distance_to_subway']
+
 import numpy as np
 import pandas as pd
 import psycopg2
@@ -6,18 +20,68 @@ from geopy.distance import geodesic
 import re
 import io
 import time
+import requests
+import h3
 warnings.simplefilter(action='ignore', category=FutureWarning)
-# =====================================================================================
+
+def lat_lng_to_h3(lat, lng, resolution=9):    # dir(h3)
+    return h3.geo_to_h3(lat, lng, resolution) # geo_to_h3 or latlng_to_cell
+
+def get_poi_distance(lat, lng, poi_type):
+    overpass_url = "https://overpass-api.de/api/interpreter"
+    radius = 10_000
+    if poi_type == "subway":
+        overpass_query = f"""
+        [out:json];
+        node["station"="subway"](around:{radius},{lat},{lng});
+        out body;
+        """
+    else:
+        overpass_query = f"""
+        [out:json];
+        node["amenity"="{poi_type}"](around:{radius},{lat},{lng});
+        out body;
+        """
+    max_retries = 5
+    retry_delay = 5  # Задержка между попытками в секундах
+    
+    for attempt in range(max_retries):
+        try:
+            response = requests.get(overpass_url, params={'data': overpass_query})
+            response.raise_for_status()  # Проверяем HTTP-статус ответа
+            data = response.json()       # Пытаемся декодировать JSON
+            break
+        except (requests.exceptions.RequestException, ValueError) as e:
+            print(f"Attempt {attempt + 1} failed: {e}")
+            if attempt < max_retries - 1:
+                time.sleep(retry_delay)
+            else:
+                print("Max retries reached. Returning default value.")
+                return -1  # Возвращаем значение по умолчанию при неудаче
+    min_distance = float('inf')
+    if len(data['elements']) > 0:
+        for element in data['elements']:
+            if 'lat' in element and 'lon' in element:
+                poi_lat = element['lat']
+                poi_lon = element['lon']
+            elif 'center' in element:
+                poi_lat = element['center']['lat']
+                poi_lon = element['center']['lon']
+            else:
+                continue
+            distance = geodesic((lat, lng), (poi_lat, poi_lon)).kilometers
+            min_distance = min(min_distance, distance)
+    else: # Если объектов не найдено, возвращаем -1 или другое значение по умолчанию
+        return -1
+    return round(min_distance, 6)
+
 def drop_high_perc_val_columns(df):
     high_perc_val_string = []
     for column in df.columns.to_list():
         perc = round(df[column].isnull().sum()/len(df[column])*100, 1)
-        if perc > 40:
-#             df = df.drop(column, axis=1)
-#             print(column, perc)
+        if perc > 20:
             high_perc_val_string.append(str(column))
-    # print('Новые признаки с большим кол-вом пропусков: ',high_perc_val_string)
-    
+    print('Новые признаки с большим кол-вом пропусков: ',high_perc_val_string)
     return df
 
 def extract_floors_info(df):
@@ -37,13 +101,9 @@ def extract_floors_info(df):
     
     return df
 
-# Функция для определения ближайшего города
 def determine_city(row):
-    # Координаты текущей точки
     point = (row['lat'], row['lng'])
-    # Вычисление расстояний до всех городов
     distances = {city: geodesic(point, coords).kilometers for city, coords in city_coords.items()}
-    # Выбор города с минимальным расстоянием
     nearest_city = min(distances, key=distances.get)
     return nearest_city
 
@@ -59,25 +119,16 @@ def fill_missing_by_lat_lng_and_address(df, col='year_buld',
                                         group_cols=['lat', 'lng'],
                                         address_col='address'
                                        ):
-
-    # 1) Считаем среднее по заданным колонкам group_cols и округляем
-    mean_vals = df.groupby(group_cols)[col].mean().round()
-
-    # 2) Промежуточная колонка для хранения среднего
-    df['_temp_fill'] = df.set_index(group_cols).index.map(mean_vals)
-    # 3) Заполняем пропуски значениями из _temp_fill
-    df[col] = df[col].fillna(df['_temp_fill'])
-    # 4) Удаляем временную колонку
-    df.drop(columns=['_temp_fill'], inplace=True)
-
-    # 5) Заполняем оставшиеся пропуски по моде в рамках одного адреса
-    #    (если вдруг для данного адреса есть хотя бы одно ненулевое значение)
+    mean_vals = df.groupby(group_cols)[col].mean().round()            # 1) Считаем среднее по заданным колонкам group_cols и округляем
+    df['_temp_fill'] = df.set_index(group_cols).index.map(mean_vals)  # 2) Промежуточная колонка для хранения среднего
+    df[col] = df[col].fillna(df['_temp_fill'])                        # 3) Заполняем пропуски значениями из _temp_fill
+    df.drop(columns=['_temp_fill'], inplace=True)                     # 4) Удаляем временную колонку
+    # 5) Заполняем оставшиеся пропуски по моде в рамках одного адреса (если вдруг для данного адреса есть хотя бы одно ненулевое значение)
     for addr in df[df[col].isnull()][address_col].unique():
         addr_vals = df.loc[df[address_col] == addr, col].value_counts()
         if not addr_vals.empty:
             most_common_val = addr_vals.idxmax()  # самое частое значение
             df.loc[(df[address_col] == addr) & (df[col].isnull()), col] = most_common_val
-
     return df
 
 def fill_missing_rooms(df):
@@ -90,7 +141,14 @@ def fill_missing_rooms(df):
         df_filled.loc[index, 'rooms_count'] = closest_rooms
     return df_filled
 
-# =====================================================================================
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import ks_2samp, ttest_ind
+
+
 DB_NAME = "rev_data"
 DB_USER = "analyst"
 DB_PASSWORD = "iPJenuTt"
@@ -101,33 +159,26 @@ with psycopg2.connect("host='{}' port={} dbname='{}' user={} password={}".format
                                                                                  , DB_NAME
                                                                                  , DB_USER
                                                                                  , DB_PASSWORD)) as conn:
-    sql = "select * from flats;"
+    sql = """  select address, area, area_kitchen, area_live,
+               price_object, renovation, rooms_count,
+               type_flat, status_flat, lat, lng,
+               year_buld, floors_count, floor, source_id,
+               parsed_at, url_link, title
+               from flats_active
+               where parsed_at >= (select max(date(parsed_at)) from flats_active)
+               and lower(title) NOT LIKE '%доля%'
+               and lower(description) NOT LIKE '%номер извещения%'
+               and lower(description) NOT LIKE '%аукцион%';"""
+    
     df = pd.read_sql_query(sql, conn)
-# ====================================================================================
-df.replace('nan', '', inplace=True)
-df.replace('', np.nan, inplace=True)
-df = df[['address', 'area', 'area_kitchen', 'area_live', 'balcony_cat', 'normalized_price',
-       'has_any_lift', 'deadline_end_bd', 'height_m', 'house_type',
-       'price_object', 'renovation', 'rooms_count', 'sanuzel_combined',
-       'sanuzel_separate', 'type_flat', 'status_flat', 'lat', 'lng',
-       'window_type', 'year_buld', 'floors_count', 'floor', 'source_id',
-       'city', 'parsed_at', 'url_link', 'description', 'title']]
-# print('Начало', df.shape)
-df = df.dropna(subset=['price_object', 'rooms_count', 'type_flat', 'lat', 'lat', 'url_link'])
-df.replace('', np.nan, inplace=True)
-df = df[df['price_object']<1_000_000_000]
-df['normalized_price'] = df['normalized_price'].fillna(round(df['price_object'] / df['area'], 2))
-# = rooms_count =====================================================
-df = fill_missing_rooms(df)
-df = df[df['rooms_count']<7]
-# print('После смерти выбрасов в rooms_count', df.shape)
-# ======================================================
+# =======================================================================================
 # Проверяем количество дубликатов
+df['source_id'] = df['source_id'].astype(int)
 duplicated_ids = df[df['source_id'].duplicated()]
-# print(f"Количество дубликатов: {len(duplicated_ids)}")
+print(f"Количество дубликатов: {len(duplicated_ids)}")
 # Удаляем дубликаты, оставляя первое вхождение
-df = df.sort_values(by=['parsed_at', 'price_object'], ascending=[False, True]).drop_duplicates(subset='source_id', keep='first')
-# print('После смерти дубликатов', df.shape)
+df = df.sort_values(by=['parsed_at', 'year_buld', 'price_object'], ascending=[False, True, True]).drop_duplicates(subset='source_id', keep='first')
+print('После смерти дубликатов', df.shape)
 # Координаты центров городов
 city_coords = {
     "Казань": (55.796127, 49.106414),
@@ -135,23 +186,62 @@ city_coords = {
     "Нижний Новгород": (56.326797, 44.006516)
 }
 df['city'] = df.apply(determine_city, axis=1)
-# =====================================================================================
-df = drop_high_perc_val_columns(df)
 df = df[(df['city'] == 'Нижний Новгород') | (df['city'] == 'Казань') | (df['city'] == 'Самара')]
+# city_mapping = {
+#     'Самара': 1,
+#     'Нижний Новгород': 2,
+#     'Казань': 3
+# }
+# df['city'] = df['city'].map(city_mapping)
+# =======================================================================================
+df.replace('nan', '', inplace=True)
+df.replace('', np.nan, inplace=True)
+df = df.dropna(subset=['price_object', 'rooms_count', 'type_flat', 'lat', 'lat', 'url_link'])
+df.replace('', np.nan, inplace=True)
+df['price_object'] = df['price_object'].astype(int)
+df = df[df['price_object']<1_000_000_000]
+
+
+# №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№
+# Копия данных перед заполнением пропусков
+data_before = df.copy()
+# №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№
+
+
+# = year_buld ===========================================================================
+df = fill_missing_by_lat_lng_and_address(df, col='year_buld')
+df = df[(df['year_buld']> 1800) & (df['year_buld']<=2025)]
+
+# = rooms_count =========================================================================
+df = fill_missing_rooms(df)
+df = df[df['rooms_count']<=7]
+# print('После смерти выбрасов в rooms_count', df.shape)
+# =======================================================================================
 df = df[df['status_flat'] != 2]
 df = df.drop(['status_flat'], axis=1)
+# print('Оставили только квартиры (без апартов)', df.shape)
 df = df[df['type_flat'] != 2]
 df = df.drop(['type_flat'], axis=1)
-# print('Оставили только квартиры (без апартов)', df.shape)
-# ===========================================================================================================================
-# = address =====================================================
+# print('Оставили только вторички (без новостроек)', df.shape)
+df = drop_high_perc_val_columns(df)
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+# = address =============================================================================
 df.loc[df['address'] == 'Казань', 'address'] = np.nan
 df.loc[df['address'] == 'Нижний Новгород', 'address'] = np.nan
 df.loc[df['address'] == 'Самара', 'address'] = np.nan
 df = df[~df['address'].isnull()]
 # print('После смерти пустых адресов', df.shape)
 
-# = area_kitchen, area_live, area =====================================================
+# = renovation ==========================================================================
+mapping = {
+    'Без ремонта': 0,
+    'Косметический': 1,
+    'Евроремонт': 2,
+    'Дизайнерский': 3
+}
+df['renovation'] = df['renovation'].map(mapping)
+
+# = area_kitchen, area_live, area =======================================================
 df['area'] = df['area'].apply(clean_area_regex)
 df['area_kitchen'] = df['area_kitchen'].apply(clean_area_regex)
 df['area_live'] = df['area_live'].apply(clean_area_regex)
@@ -162,90 +252,28 @@ df = df[df['area']>10]
 df['area_kitchen'] = df['area_kitchen'].fillna(df['area'] - df['area_live'])
 # Заполняем пропуски в area_live
 df['area_live'] = df['area_live'].fillna(df['area'] - df['area_kitchen'])
-
 # оставшиеся пропуски заменим значением, проппорциональным среднему по заполненным в отношении в общей площади
 # отношения площади гостиной и кухни к общей площади
 df['area_live_ratio'] = df['area_live'] / df['area']
 df['area_kitchen_ratio'] = df['area_kitchen'] / df['area']
+
 df['area_live'] = df['area_live'].fillna(df['area'] * df['area_live_ratio'].median())
 df['area_kitchen'] = df['area_kitchen'].fillna(df['area'] * df['area_kitchen_ratio'].median())
+
 df['area_live_ratio'] = df['area_live'] / df['area']
 df['area_kitchen_ratio'] = df['area_kitchen'] / df['area']
 
-df = df.drop(['deadline_end_bd'], axis=1)
-df = fill_missing_by_lat_lng_and_address(df, col='year_buld')
+# df['combined_ratio'] = df['area_live_ratio'] * df['area_kitchen_ratio']
+# Высокое значение combined_ratio может указывать на квартиру с большим жилым пространством и удобной кухней
+# Низкое значение может говорить о том, что квартира имеет много "нежилых" зон (например, коридоры, кладовки)
+df['area_per_renovation'] = df['area'] * (df['renovation'] + 1)
 
-mode_value = df['year_buld'].mode()[0]
-df['year_buld'] = df['year_buld'].fillna(mode_value)
-df = df[df['year_buld']>1800]
-# print('После смерти выбрасов в year_buld', df.shape)
-
-# ====================================================================================================
-# = floors_count,floor =====================================================
+# = floors_count ========================================================================
 df = extract_floors_info(df)
-df['floor_ratio'] = df['floor']/df['floors_count']
-df['floor_to_live_ratio'] = df['floor_ratio'] / df['area_live_ratio'].replace(0, 1e-10)
-df = df[(df['floors_count']<80) & (df['floors_count']>0)]
+# df = df[(df['floors_count']<80) & (df['floors_count']>0)]
 # print('После смерти выбрасов в floors_count', df.shape)
-# ====================================================================================================
 
-# = has_any_lift =====================================================
-df = fill_missing_by_lat_lng_and_address(df, col='has_any_lift')
-df.loc[(df['has_any_lift'].isna()) & (df['floors_count'] > 5), 'has_any_lift'] = 1
-df.loc[(df['has_any_lift'].isna()) & (df['floors_count'] <= 5), 'has_any_lift'] = 0
-
-# # = renovation =====================================================
-# # 0 - Без ремонта, 1 - косметический, 2 - евро, 3 - дизайнерский
-df['renovation'] = df['renovation'].replace('Без ремонта', 0).replace('Косметический', 1).replace('Евроремонт', 2).replace('Дизайнерский', 3).replace('Требуется', 4)
-df.loc[df['renovation'].isna(), 'renovation'] = 4
-df['renovation'] = df['renovation'].astype(int)
-
-# = sanuzel_combined, sanuzel_separate =====================================================
-# Инициализируем колонку
-# df['sanuzel_multiple'] = 0
-# Условие 0: если его нет (ну мало ли)
-df.loc[(df['sanuzel_combined'] == 0) & (df['sanuzel_separate'] == 0), 'sanuzel_multiple'] = 0
-# Условие 1: только раздельный
-df.loc[(df['sanuzel_combined'] == 0) & (df['sanuzel_separate'] == 1), 'sanuzel_multiple'] = 1
-# Условие 2: только совмещённый
-df.loc[(df['sanuzel_combined'] == 1) & (df['sanuzel_separate'] == 0), 'sanuzel_multiple'] = 1
-# Условие 3: и совмещённый, и раздельный
-df.loc[(df['sanuzel_combined'] == 1) & (df['sanuzel_separate'] == 1), 'sanuzel_multiple'] = 2
-df = df.drop(['sanuzel_combined', 'sanuzel_separate'], axis=1)
-df['sanuzel_per_room'] = df['rooms_count']/ df['sanuzel_multiple']
-df['sanuzel_multiple'] = df['sanuzel_multiple'].astype(int)
-
-# = height_m =====================================================
-def scale_to_10(x):
-    while x > 10:
-        x /= 10
-    return x
-df['height_m'] = df['height_m'].apply(scale_to_10)
-df = fill_missing_by_lat_lng_and_address(df, col='height_m')
-mode_value = df['height_m'].mode()[0]
-df['height_m'] = df['height_m'].fillna(mode_value)
-
-# = balcony_cat =====================================================
-mode_value = df['balcony_cat'].mode()[0]
-df['balcony_cat'] = df['balcony_cat'].fillna(mode_value)
-
-# = window_type =====================================================
-df = fill_missing_by_lat_lng_and_address(df, col='window_type')
-mode_value = df['window_type'].mode()[0]
-df['window_type'] = df['window_type'].fillna(mode_value)
-
-# = house_type =====================================================
-mode_value = df['house_type'].mode()[0]
-df['house_type'] = df['house_type'].fillna(mode_value)
-df['house_type'] = df['house_type'].astype('category')
-df['house_type'] = df['house_type'].cat.codes
-
-# ===========================================================================================================================
-df = df.drop('address', axis=1)
-df['price_object'] = df['price_object'].astype(int)
-# print('000', df.shape)
-
-# = distance_to_center =====================================================
+# = distance_to_center ==================================================================
 df.loc[df['city'] == 'Нижний Новгород', 'center_lat'] = 56.326797
 df.loc[df['city'] == 'Нижний Новгород', 'center_lng'] = 44.006516
 df.loc[df['city'] == 'Казань', 'center_lat'] = 55.796127
@@ -257,12 +285,55 @@ df['distance_to_center'] = df.apply(
     axis=1
 )
 df = df.loc[df['distance_to_center'] <= 100]
-df = df.drop(['center_lat', 'center_lng', 'lat', 'lng', 'title'], axis=1)
 
-# = description_length =====================================================
-# Количество слов в описании
-df['description_length'] = df['description'].apply(lambda x: len(str(x).split()) if pd.notnull(x) else 0)
-df = df.drop(['description'], axis=1)
+# = h3_categorical ======================================================================
+df['h3_categorical'] = df.apply(lambda row: lat_lng_to_h3(row['lat'], row['lng']), axis=1).astype('category').cat.codes
+
+# = distance_to_subway ==================================================================
+subway_dist_df = pd.read_csv('subway_dist_df_250508.csv')
+df['id'] = df['source_id']
+df.set_index('id', inplace=True)
+subway_dist_df.set_index('id', inplace=True)
+df = df.join(subway_dist_df['distance_to_subway'], how='left')
+# для новых данных, в которых значение не заполнено
+start_time = time.time()
+print(start_time)
+df['distance_to_subway'] = df.apply(
+    lambda x: get_poi_distance(x['lat'], x['lng'], 'subway') 
+              if pd.isna(x['distance_to_subway']) else x['distance_to_subway'], 
+    axis=1
+)
+print('Время расчета: ', round((time.time() - start_time)/60/60, 2), ' ч')
+# ------------------------------------------------------------------------------------------------------------------------------------------------------
+df = df.drop(['address', 'renovation', 'lat', 'lng', 'floor', 'title', 'center_lat', 'center_lng'], axis=1)
+print('ready')
+
+df_df_df = df[['source_id','distance_to_subway']]
+df_df_df['id'] = df_df_df['source_id']
+df_df_df.to_csv('subway_dist_df_250508.csv', index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # ===========================================================================================================
